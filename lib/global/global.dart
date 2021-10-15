191438b43f41bcc47f827e3bb5f4a6e2cb510398
wuxjian <xjwu@csih.cn>
2021-10-15 18:03:48 +0800

    添加global类

---
+++ lib/global/global.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+class Global {
+  Dio dio;
+
+  Global._privateConstructor() : dio = Dio() {
+    dio.options = BaseOptions(
+      baseUrl: "https://zxw.td0f7.cn/",
+      connectTimeout: 5000,
+      sendTimeout: 5000,
+      receiveTimeout: 5000,
+      contentType: Headers.formUrlEncodedContentType,
+      responseType: ResponseType.json,
+    );
+
+    dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) {
+        EasyLoading.show(status: "Loading...");
+        return handler.next(options); //continue
+      },
+      onResponse: (response, handler) {
+        EasyLoading.dismiss();
+        return handler.next(response); // continue
+      },
+      onError: (DioError e, handler) {
+        print(e.toString());
+        EasyLoading.dismiss();
+        String msg = "";
+        if (e.type == DioErrorType.connectTimeout) {
+          msg = "连接超时错误";
+        } else {
+          msg = "接口错误！";
+        }
+        EasyLoading.showError(msg);
+      },
+    ));
+
+  }
+
+  static final Global _instance = Global._privateConstructor();
+
+  Global get instance => _instance;
+}
